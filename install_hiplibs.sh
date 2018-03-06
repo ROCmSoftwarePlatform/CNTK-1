faa1f78d5db8901e2b2624c95ab052e4ac33578b
Prasandhmcw <prasandh@multicorewareinc.com>
2018-03-06 18:42:22 +0530

    installation script changes to accomodate both AMD and NVIDIA platforms

--- install_hiplibs.sh
+++ install_hiplibs.sh
@@ -12,10 +12,13 @@ cur_dir=$(pwd)
 mkdir lib64 -p
 
 #List of repos to be cloned and installed
-repoList=(hipBLAS rocRAND HcSPARSE hipDNN)
+repoList=(hipBLAS rocRAND HcSPARSE)
 
 #Installation directories
-installDir=("hipblas" " " "hcsparse" "hipdnn")
+installDir=("hipblas" " " "hcsparse")
+
+#repoList+=(hcBLAS)
+#installDir+=("hcblas")
 
 #git command
 clone="git clone https://github.com/ROCmSoftwarePlatform"
@@ -66,9 +69,22 @@ else
 fi
 
 export HIP_PATH="$rootDir/$externalDir/hip"
+HIP_PATH="$rootDir/$externalDir/hip"
 
 #platform deducing
 platform=$($rootDir/$externalDir/hip/bin/hipconfig --platform)
+
+dependencies=("make" "cmake-curses-gui" "pkg-config")
+if [ "$platform" == "hcc" ]; then
+	dependencies+=("python2.7" "python-yaml" "libssl-dev" "libboost-dev" "libboost-system-dev" "libboost-filesystem-dev")
+fi
+
+for package in "${dependencies[@]}"; do
+    if [[ $(dpkg-query --show --showformat='${db:Status-Abbrev}\n' ${package} 2> /dev/null | grep -q "ii"; echo $?) -ne 0 ]]; then
+      printf "\033[32mInstalling \033[33m${package}\033[32m from distro package manager\033[0m\n"
+      sudo apt install -y --no-install-recommends ${package}
+    fi
+done
 
 #extra repos for hcc
 if [ "$platform" == "hcc" ]; then
@@ -83,11 +99,43 @@ if [ "$platform" == "hcc" ]; then
 	else
 		echo -e "\n--------------------- CLONING rocBLAS ---------------------\n"
 		$clone/rocBLAS.git
+		echo -e "\n--------------------- INSTALLING rocBLAS ---------------------\n"
                 cd rocBLAS && mkdir $build_dir -p && cd $build_dir
                 $cmake_it/rocblas .. && make && make install
                 cd $rootDir/$externalDir
 	fi
+	export rocblas_DIR=$rootDir/$externalDir/rocblas/lib/cmake/rocblas
+#dependencies for miopengemm
+
+	#opencl
+	#sudo apt update
+	sudo apt install ocl-icd-opencl-dev
+
+	#rocm make package
+	git clone https://github.com/RadeonOpenCompute/rocm-cmake.git
+	cd rocm-cmake
+	mkdir $build_dir -p && cd $build_dir
+	$cmake_it/ ..
+	cmake --build . --target install
+	export ROCM_DIR=$(pwd)/../share/rocm/cmake/
+	cd $rootDir/$externalDir
+
+#dependencies for miopen
+
+	#clang-ocl
+	git clone https://github.com/RadeonOpenCompute/clang-ocl.git
+	cd clang-ocl
+	mkdir $build_dir -p && cd $build_dir
+	$cmake_it/ ..
+	cmake --build . --target install
+	cd $rootDir/$externalDir
+
+	#ssl
+	#sudo apt-get install libssl-dev
 fi
+
+repoList+=(hipDNN)
+installDir+=("hipdnn")
 
 #cloning and install
 for i in "${!repoList[@]}"
@@ -104,9 +152,14 @@ do
         if [ "${repoList[$i]}" == "rocRAND" ]; then
             git checkout rocm_1_7_1
         fi
-        if [ "${repoList[$i]}" != "hipDNN" ]; then
+        echo -e "\n--------------------- INSTALLING ${repoList[$i]} ---------------------\n"
+        if [ "${repoList[$i]}" != "hipDNN" ] && [ "${repoList[$i]}" != "MIOpen" ]; then
             mkdir $build_dir -p && cd $build_dir
             $cmake_it/${installDir[$i]} ${build_test[$i]} .. && make && make install
+	    elif [ "${repoList[$i]}" == "MIOpen" ]; then
+	        export miopengemm_DIR=$rootDir/$externalDir/miopengemm/lib/cmake/miopengemm
+            mkdir $build_dir -p && cd $build_dir
+            CXX=/opt/rocm/hcc/bin/hcc cmake -DMIOPEN_BACKEND=HIP -DCMAKE_PREFIX_PATH="/opt/rocm/hcc;$(HIP_PATH)" -DCMAKE_CXX_FLAGS="-isystem /usr/include/x86_64-linux-gnu/" -DCMAKE_INSTALL_PREFIX=../../miopen .. && make && make install
         else
             make INSTALL_DIR=../hipdnn HIP_PATH=$rootDir/$externalDir/hip
         fi
@@ -122,6 +175,7 @@ else
     git clone https://github.com/ROCmSoftwarePlatform/cub-hip.git
     cd cub-hip
     git checkout hip_port_1.7.4
+    #git checkout 3effedd23f4e80ccec5d0808d8349f7d570e488e
     cd $rootDir/$externalDir
 fi
 
